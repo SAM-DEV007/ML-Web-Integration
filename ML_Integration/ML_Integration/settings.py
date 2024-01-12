2f23602a3811f3e56de567406035bf4196ebaf63
SAM <8dmasters@gmail.com>
2024-01-12 20:53:08 +0530

    Added 404

--- ML_Integration/ML_Integration/settings.py
+++ ML_Integration/ML_Integration/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'ML_Integration.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'main/templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
