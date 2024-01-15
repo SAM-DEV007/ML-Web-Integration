1fbbf922892b88428c8437fdeb3b31847c06626f
SAM <8dmasters@gmail.com>
2024-01-16 01:34:41 +0530

    Update views.py

--- ML_Integration/instagram_filters/views.py
+++ ML_Integration/instagram_filters/views.py
@@ -3,6 +3,16 @@ from django.http import HttpResponse
 from django.template import loader
 
 
-def home(request):
-    template = loader.get_template('home.html')
+def flappy_bird(request):
+    template = loader.get_template('flappy_bird.html')
+    return HttpResponse(template.render({}, request))
+
+
+def maths_equation(request):
+    template = loader.get_template('maths_equation.html')
+    return HttpResponse(template.render({}, request))
+
+
+def hand_gesture(request):
+    template = loader.get_template('hand_gesture.html')
     return HttpResponse(template.render({}, request))
