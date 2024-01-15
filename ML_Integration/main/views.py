5f6840c422a3041bd9cce5cc2a2c9e0c96fc04b3
SAM <8dmasters@gmail.com>
2024-01-16 03:24:45 +0530

    Added custom start

--- ML_Integration/main/views.py
+++ ML_Integration/main/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 from django.template import loader
 
 
-def home(request):
+def home(request, context={'igstat': 1, 'rightshift': 0}):
     template = loader.get_template('home.html')
-    return HttpResponse(template.render({}, request))
+    return HttpResponse(template.render(context, request))
