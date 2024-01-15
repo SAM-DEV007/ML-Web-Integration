eee1caecbbd8d963abfc06ae0de89605f371f6d7
SAM <8dmasters@gmail.com>
2024-01-16 01:36:40 +0530

    Update urls.py

--- ML_Integration/instagram_filters/urls.py
+++ ML_Integration/instagram_filters/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='main'),
+    path('flappy_bird/', views.flappy_bird, name='flappy_bird'),
+    path('maths_equation/', views.maths_equation, name='maths_equation'),
+    path('hand_gesture/', views.hand_gesture, name='hand_gesture'),
 ]
