c6311a60731a19fccc496335d7a261536e29194e
SAM <8dmasters@gmail.com>
2024-01-17 01:57:05 +0530

    Segregated contents of the script

--- ML_Integration/instagram_filters/hand_gesture.py
+++ ML_Integration/instagram_filters/hand_gesture.py
@@ -11,8 +11,17 @@ import os
 import time
 
 
+def flappy_gen():
+    obj = HandGesture()
+
+    while True:
+        frame = obj.main()
+        yield (b'--frame\r\n'
+				b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
 class predict():
-    def __init__(self, model_path = os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__))) + '\\Model\\Model_Data\\', 'Model.tflite'), num_threads = 1):
+    def __init__(self, model_path = settings.BASE_DIR / 'instagram_filters/HandGesture_Data/Model.tflite', num_threads = 1):
         self.interpreter = tf.lite.Interpreter(model_path = model_path, num_threads = num_threads)
 
         self.interpreter.allocate_tensors()
@@ -35,219 +44,193 @@ class predict():
         return result_index
 
 
-def check_dir():
-    '''Checks if the Captured Video directory exists'''
+class HandGesture():
+    def __init__(self):
+        cam = cv2.VideoCapture(0)
+        cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-    path = os.path.abspath(os.path.join(os.path.dirname(__file__))) + '\\Captured Video\\'
-    if not os.path.exists(path):
-        os.makedirs(path)
+        show_palm = True
+        show_ok = True
+        show_peace = True
 
+        img_busy = False
 
-def get_num() -> int:
-    '''Gets the no. of the last recorded file'''
+        model_db = 0
+        gesture = None
 
-    num = -1
+        mphands = mp.solutions.hands
+        hands = mphands.Hands()
 
-    for file in os.listdir(os.path.abspath(os.path.join(os.path.dirname(__file__))) + '\\Captured Video\\'):
-        if os.path.splitext(file)[-1] == '.mp4':
-            if 'record_' in file:
-                file = file.replace('.mp4', '')
-                t_num = int(file.split('_')[1])
-                if t_num > num:
-                    num = t_num
-    
-    return num
+        model_predict = predict()
 
+        mask_big_img = None
+        big_img = None
+        big_img_time = None
 
-def landmark_list(image, landmarks):
-    '''Generates the landmark list'''
+        mask_palm_img_holder = None
+        mask_ok_img_holder = None
+        mask_peace_img_holder = None
 
-    image_width, image_height = image.shape[1], image.shape[0]
+        palm_img_holder = None
+        ok_img_holder = None
+        peace_img_holder = None
 
-    landmark_point = []
+        temp_mask_palm_img_holder = None
+        temp_mask_ok_img_holder = None
+        temp_mask_peace_img_holder = None
 
-    for _, landmark in enumerate(landmarks.landmark):
-        landmark_x = min(int(landmark.x * image_width), image_width - 1)
-        landmark_y = min(int(landmark.y * image_height), image_height - 1)
+        temp_palm_img_holder = None
+        temp_ok_img_holder = None
+        temp_peace_img_holder = None
 
-        landmark_point.append([landmark_x, landmark_y])
+        img_size = 150
 
-    return pre_process_data(landmark_point)
+        palm_path = os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__))) + '\\Data\\', 'Palm.png')
+        ok_path = os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__))) + '\\Data\\', 'Ok.png')
+        peace_path = os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__))) + '\\Data\\', 'Peace.png')
 
+        size = 100
 
-def pre_process_data(landmark_list):
-    '''Pre processes the data for the trained model'''
-
-    temp_landmark_list = copy.deepcopy(landmark_list)
-
-    base_x, base_y = 0, 0
-    for index, landmark_point in enumerate(temp_landmark_list):
-        if index == 0:
-            base_x, base_y = landmark_point[0], landmark_point[1]
-
-        temp_landmark_list[index][0] = temp_landmark_list[index][0] - base_x
-        temp_landmark_list[index][1] = temp_landmark_list[index][1] - base_y
-
-    temp_landmark_list = list(itertools.chain.from_iterable(temp_landmark_list))
-
-    max_value = max(list(map(abs, temp_landmark_list)))
-
-    def normalize_(n):
-        return n / max_value
-
-    temp_landmark_list = list(map(normalize_, temp_landmark_list))
-
-    return temp_landmark_list
-
-
-def detect_hands(frame, hands, model_predict):
-    '''Detects the hand and returns the suitable gesture'''
-
-    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    result = hands.process(rgb)
-
-    hand_landmarks = result.multi_hand_landmarks
-    gesture = None
-
-    if hand_landmarks:
-        for handLMs in hand_landmarks:
-            if handLMs:
-                landmark = landmark_list(frame, handLMs)
-                gesture = model_predict(landmark)
-    
-    if gesture == 3: gesture = None
-    return gesture
-
-
-def mask(img):
-    '''Masks the image'''
-
-    img2gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    _, mask = cv2.threshold(img2gray, 1, 255, cv2.THRESH_BINARY)
-
-    return mask
-
-
-def pre_img(frame, size, img, mask_img, gesture):
-    '''Initializes the hand img to the frame'''
-
-    img_section = [-600, -360, -130]
-    img_x = img_section[gesture]
-
-    roi = frame[-465: -465+size, img_x: img_x+size]
-  
-    roi[np.where(mask_img)] = 0
-    roi += img
-
-
-def get_img(frame):
-    '''Gets the images after the gesture'''
-
-    cv2.rectangle(frame, [130, 170], [510, 450], (0, 0, 0), 2)
-
-    cv2.rectangle(frame, [110, 150], [530, 170], (255, 255, 255), -1)
-    cv2.rectangle(frame, [110, 450], [530, 470], (255, 255, 255), -1)
-    cv2.rectangle(frame, [110, 170], [130, 450], (255, 255, 255), -1)
-    cv2.rectangle(frame, [510, 170], [530, 450], (255, 255, 255), -1)
-
-    cv2.rectangle(frame, [110, 150], [530, 470], (0, 0, 0), 2)
+        palm = cv2.imread(palm_path)
+        palm = cv2.resize(palm, (size, size))
+        mask_palm = mask(palm)
         
-    img = frame[149: 472, 109: 532] 
+        ok = cv2.imread(ok_path)
+        ok = cv2.resize(ok, (size, size))
+        mask_ok = mask(ok)
 
-    return img
+        peace = cv2.imread(peace_path)
+        peace = cv2.resize(peace, (size, size))
+        mask_peace = mask(peace)
 
-
-def hold_img_main(frame, img, mask_img):
-    '''Holds the img on the screen for a while'''
-
-    roi = frame[149: 472, 109: 532]
-    roi[np.where(mask_img)] = 0
-
-    roi += img
-
-
-def change_img(frame, img, mask_img, gesture: int, img_size: int):
-    '''Changes the gesture image'''
-
-    img_section = [-600, -400, -200]
-    img_x = img_section[gesture]
-
-    roi = frame[-465: -465+img_size, img_x: img_x+img_size]
-    roi[np.where(mask_img)] = 0
-
-    roi += img
-
-
-def main():
-    '''The main function to start the filter'''
-
-    cam = cv2.VideoCapture(0)
-    cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-
-    vid_cod = cv2.VideoWriter_fourcc(*'mp4v') 
-
-    check_dir() 
-    i = get_num()
-    fps = 20
-    save_path = os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__))) + '\\Captured Video\\', f'record_{i+1}.mp4')
-    output = cv2.VideoWriter(save_path, vid_cod, fps, (640,480))
-
-    show_palm = True
-    show_ok = True
-    show_peace = True
-
-    img_busy = False
-
-    model_db = 0
-    gesture = None
-
-    mphands = mp.solutions.hands
-    hands = mphands.Hands()
-
-    model_predict = predict()
-
-    mask_big_img = None
-    big_img = None
-    big_img_time = None
-
-    mask_palm_img_holder = None
-    mask_ok_img_holder = None
-    mask_peace_img_holder = None
-
-    palm_img_holder = None
-    ok_img_holder = None
-    peace_img_holder = None
-
-    temp_mask_palm_img_holder = None
-    temp_mask_ok_img_holder = None
-    temp_mask_peace_img_holder = None
-
-    temp_palm_img_holder = None
-    temp_ok_img_holder = None
-    temp_peace_img_holder = None
-
-    img_size = 150
-
-    palm_path = os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__))) + '\\Data\\', 'Palm.png')
-    ok_path = os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__))) + '\\Data\\', 'Ok.png')
-    peace_path = os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__))) + '\\Data\\', 'Peace.png')
-
-    size = 100
-
-    palm = cv2.imread(palm_path)
-    palm = cv2.resize(palm, (size, size))
-    mask_palm = mask(palm)
     
-    ok = cv2.imread(ok_path)
-    ok = cv2.resize(ok, (size, size))
-    mask_ok = mask(ok)
+    def __del__(slef):
+        cv2.destroyAllWindows()
 
-    peace = cv2.imread(peace_path)
-    peace = cv2.resize(peace, (size, size))
-    mask_peace = mask(peace)
 
-    while True:
+    def landmark_list(image, landmarks):
+        '''Generates the landmark list'''
+
+        image_width, image_height = image.shape[1], image.shape[0]
+
+        landmark_point = []
+
+        for _, landmark in enumerate(landmarks.landmark):
+            landmark_x = min(int(landmark.x * image_width), image_width - 1)
+            landmark_y = min(int(landmark.y * image_height), image_height - 1)
+
+            landmark_point.append([landmark_x, landmark_y])
+
+        return pre_process_data(landmark_point)
+
+
+    def pre_process_data(landmark_list):
+        '''Pre processes the data for the trained model'''
+
+        temp_landmark_list = copy.deepcopy(landmark_list)
+
+        base_x, base_y = 0, 0
+        for index, landmark_point in enumerate(temp_landmark_list):
+            if index == 0:
+                base_x, base_y = landmark_point[0], landmark_point[1]
+
+            temp_landmark_list[index][0] = temp_landmark_list[index][0] - base_x
+            temp_landmark_list[index][1] = temp_landmark_list[index][1] - base_y
+
+        temp_landmark_list = list(itertools.chain.from_iterable(temp_landmark_list))
+
+        max_value = max(list(map(abs, temp_landmark_list)))
+
+        def normalize_(n):
+            return n / max_value
+
+        temp_landmark_list = list(map(normalize_, temp_landmark_list))
+
+        return temp_landmark_list
+
+
+    def detect_hands(frame, hands, model_predict):
+        '''Detects the hand and returns the suitable gesture'''
+
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        result = hands.process(rgb)
+
+        hand_landmarks = result.multi_hand_landmarks
+        gesture = None
+
+        if hand_landmarks:
+            for handLMs in hand_landmarks:
+                if handLMs:
+                    landmark = landmark_list(frame, handLMs)
+                    gesture = model_predict(landmark)
+        
+        if gesture == 3: gesture = None
+        return gesture
+
+
+    def mask(img):
+        '''Masks the image'''
+
+        img2gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        _, mask = cv2.threshold(img2gray, 1, 255, cv2.THRESH_BINARY)
+
+        return mask
+
+
+    def pre_img(frame, size, img, mask_img, gesture):
+        '''Initializes the hand img to the frame'''
+
+        img_section = [-600, -360, -130]
+        img_x = img_section[gesture]
+
+        roi = frame[-465: -465+size, img_x: img_x+size]
+    
+        roi[np.where(mask_img)] = 0
+        roi += img
+
+
+    def get_img(frame):
+        '''Gets the images after the gesture'''
+
+        cv2.rectangle(frame, [130, 170], [510, 450], (0, 0, 0), 2)
+
+        cv2.rectangle(frame, [110, 150], [530, 170], (255, 255, 255), -1)
+        cv2.rectangle(frame, [110, 450], [530, 470], (255, 255, 255), -1)
+        cv2.rectangle(frame, [110, 170], [130, 450], (255, 255, 255), -1)
+        cv2.rectangle(frame, [510, 170], [530, 450], (255, 255, 255), -1)
+
+        cv2.rectangle(frame, [110, 150], [530, 470], (0, 0, 0), 2)
+            
+        img = frame[149: 472, 109: 532] 
+
+        return img
+
+
+    def hold_img_main(frame, img, mask_img):
+        '''Holds the img on the screen for a while'''
+
+        roi = frame[149: 472, 109: 532]
+        roi[np.where(mask_img)] = 0
+
+        roi += img
+
+
+    def change_img(frame, img, mask_img, gesture: int, img_size: int):
+        '''Changes the gesture image'''
+
+        img_section = [-600, -400, -200]
+        img_x = img_section[gesture]
+
+        roi = frame[-465: -465+img_size, img_x: img_x+img_size]
+        roi[np.where(mask_img)] = 0
+
+        roi += img
+
+
+    def main():
+        '''The main function to start the filter'''
+
         _, frame = cam.read() 
         frame = cv2.flip(frame, 1) 
 
@@ -323,24 +306,7 @@ def main():
             elif gesture == 2:
                 temp_peace_img_holder = img
                 temp_mask_peace_img_holder = mask(img)
-            gesture = None 
+            gesture = None
 
-        cv2.imshow('Cam', frame) 
-        output.write(frame) 
-        
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-        
-        if cv2.waitKey(1) == 27:
-            break
-        
-        if cv2.getWindowProperty('Cam', cv2.WND_PROP_VISIBLE) < 1:
-            break
-    
-    cam.release()
-    output.release()
-    cv2.destroyAllWindows()
-
-
-if __name__ == '__main__':
-    main()
+        _, jpeg = cv2.imencode('.jpg', self.frame)
+        return jpeg.tobytes()
