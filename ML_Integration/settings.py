302caa5109c4f92d571ed8254799a32e654a107e
SAM <8dmasters@gmail.com>
2024-01-19 03:29:50 +0530

    Added hosts

--- ML_Integration/settings.py
+++ ML_Integration/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-89r#=^k6eqv!e9oym5sn!-sjquj**abfuna!mct19rcxg95wcg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
