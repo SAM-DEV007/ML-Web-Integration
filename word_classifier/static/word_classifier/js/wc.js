148364debd51bb1f3b63a49cb868e0c47bd2cabd
SAM <8dmasters@gmail.com>
2024-01-22 03:32:47 +0530

    Added limit counter

--- word_classifier/static/word_classifier/js/wc.js
+++ word_classifier/static/word_classifier/js/wc.js
@@ -1,18 +1,26 @@
-$(document).on('click', '#predict', function() {
-    $.ajax({
-        url: $('#url').val(),
-        data: {
-            sentence: $('#sentence').val(),
-            csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
-        },
-        type: 'POST',
-        success: function(response) {
-            $('#predictsen').prop('readonly', false);
-            $('#predictsen').val(response);
-            $('#predictsen').prop('readonly', true);
-        },
-        error: function(error) {
-            console.log(error);
-        }
+$(document).ready(function(){
+    $('#limit').text(100);
+    
+    $("#sentence").on("input", function(){
+        $('#limit').text(100 - $(this).val().length);
+    });
+
+    $('#predict').click(function(){
+        $.ajax({
+            url: $('#url').val(),
+            data: {
+                sentence: $('#sentence').val(),
+                csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
+            },
+            type: 'POST',
+            success: function(response) {
+                $('#predictsen').prop('readonly', false);
+                $('#predictsen').val(response);
+                $('#predictsen').prop('readonly', true);
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
     });
 });
