e3512676d6690abfd4974de19ac1c1745d186e1a
SAM <8dmasters@gmail.com>
2024-01-22 20:07:12 +0530

    Info for efficient model prediction

--- word_classifier/word_classifier.py
+++ word_classifier/word_classifier.py
@@ -16,9 +16,10 @@ if not os.path.exists(model_path):
     
     from word_classifier import download_model
 
+# model = tf.keras.models.load_model(model_path, custom_objects={'KerasLayer': hub.KerasLayer}, compile=False) # Uncomment this line if using sufficient memory for the model to load (saves model prediction time)
 
 def predict(txt: str) -> str:
-    model = tf.keras.models.load_model(model_path, custom_objects={'KerasLayer': hub.KerasLayer}, compile=False)
+    model = tf.keras.models.load_model(model_path, custom_objects={'KerasLayer': hub.KerasLayer}, compile=False) # Comment this line if the above same line is uncommented
     result_arg = (np.squeeze(model.predict([txt])))
     result = np.argmax(result_arg)
 
