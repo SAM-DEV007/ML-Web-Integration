a91660662f47dc6728fe7693a0e7de33e9f711db
SAM <8dmasters@gmail.com>
2024-08-03 02:30:16 +0530

    Improved the restart functionality

--- instagram_filters/static/instagram_filters/js/flappy_bird.js
+++ instagram_filters/static/instagram_filters/js/flappy_bird.js
@@ -1,54 +1,68 @@
 $(document).ready(function(){
     let video = document.getElementById('tempvid');
     let img = document.getElementById('video');
-    let ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
-    let streaming = false;
 
-    let ws = new WebSocket(
-        ws_scheme + '://' + window.location.host + '/flappy_bird'
-    );
+    let ws;
+    let ws_scheme;
+    let streaming;
 
-    ws.onmessage = (event) => {
-        frameUpdate = event.data;
-        img.src = "data:image/jpeg;base64," + frameUpdate; 
-    };
+    const openSocket = () => {
+        ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
+        streaming = false;
 
-    if (navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({video: true, audio: false})
-        .then(function(stream){
-            video.srcObject = stream;
-            video.play();
-        })
-        .catch(function(error){
-            console.log(error);
-        });
-    }
+        ws = new WebSocket(
+            ws_scheme + '://' + window.location.host + '/flappy_bird'
+        );
 
-    video.addEventListener('canplay', function(ev){
-        video.setAttribute('width', 640);
-        video.setAttribute('height', 480);
-        $('#canvas').attr('width', 640);
-        $('#canvas').attr('height', 480);
+        ws.onmessage = (event) => {
+            frameUpdate = event.data;
+            img.src = "data:image/jpeg;base64," + frameUpdate; 
+        };
 
-        streaming = true;
-    }, false);
+        if (navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices.getUserMedia({video: true, audio: false})
+            .then(function(stream){
+                video.srcObject = stream;
+                video.play();
+            })
+            .catch(function(error){
+                console.log(error);
+            });
+        }
 
-    setInterval(main, 100);
+        video.addEventListener('canplay', function(ev){
+            video.setAttribute('width', 640);
+            video.setAttribute('height', 480);
+            $('#canvas').attr('width', 640);
+            $('#canvas').attr('height', 480);
 
-    function main(){
-        if (streaming){
-            getframe();
+            streaming = true;
+        }, false);
+
+        setInterval(main, 100);
+
+        function main(){
+            if (streaming){
+                getframe();
+            }
+        }
+
+        function getframe(){
+            let context = $('#canvas')[0].getContext('2d');
+            context.drawImage(video, 0, 0, 640, 480);
+
+            $('#canvas')[0].toBlob(function(blob) {
+                if (ws.readyState == WebSocket.OPEN) {
+                    ws.send(blob);
+                }
+            }, 'image/jpeg');
         }
     }
 
-    function getframe(){
-        let context = $('#canvas')[0].getContext('2d');
-        context.drawImage(video, 0, 0, 640, 480);
-
-        $('#canvas')[0].toBlob(function(blob) {
-            if (ws.readyState == WebSocket.OPEN) {
-                ws.send(blob);
-            }
-        }, 'image/jpeg');
-    }
+    openSocket();
+    
+    $('#restart').click(function(e){
+        e.preventDefault();
+        location.reload();
+    });
 });
